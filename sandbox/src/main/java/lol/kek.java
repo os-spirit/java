6e279dc8ba80c6506125559a549a184dba713bb1
OS-Spirit <valentin.nalivayko@gmail.com>
2020-12-23 09:42:01 +0200

    12345

--- sandbox/src/main/java/lol/kek.java
+++ sandbox/src/main/java/lol/kek.java
@@ -4,6 +4,15 @@ public class kek {
   public static void main(String[] args) {
 
     System.out.println("Hello World!");
-    System.out.println(3 + 1);
+    System.out.println(2 + 2);
+    System.out.println(2 * 2);
+    System.out.println(2 / 2);
+    System.out.println(2 - 2);
+
+    System.out.println(1 / 2);
+    System.out.println(1.0 / 2);
+
+    System.out.println("2" + "2");
+    System.out.println("2 + 2 =" + (2 + 2));
   }
 }
