48565bf82f0d0886fd41b210d6f977c9751ecb60
OS-Spirit <valentin.nalivayko@gmail.com>
2020-12-23 09:05:01 +0200

    1234
    12345

---
+++ sandbox/src/main/java/lol/kek.java
@@ -0,0 +1,9 @@
+package lol;
+
+public class kek {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println(3 + 1);
+    }
+}
--- sandbox/src/main/java/myfirstprogram.java
+++ sandbox/src/main/java/myfirstprogram.java
@@ -1,7 +1,3 @@
 public class myfirstprogram {
 
-	public static void main(String[] args) {
-
-		System.out.println("Hello World!");
-	}
 }
