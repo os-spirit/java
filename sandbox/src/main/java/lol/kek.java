efd989d7cb3205ba3f641c06bae86d16ef379021
OS-Spirit <valentin.nalivayko@gmail.com>
2020-12-25 19:43:46 +0200

    123412

--- sandbox/src/main/java/lol/kek.java
+++ sandbox/src/main/java/lol/kek.java
@@ -4,6 +4,14 @@ public class kek {
   public static void main(String[] args) {
 
     System.out.println("Hello World!");
+    hello("1234");
+   /* int l = 6;
+    int s = l * l;
+    System.out.println("ПКСС: " + l + " = " + s);
+    System.out.println("Площадь квадрата со стороной:" + 5 + " = " + (5 * 5));
+
+
+
     System.out.println(2 + 2);
     System.out.println(2 * 2);
     System.out.println(2 / 2);
@@ -13,6 +21,12 @@ public class kek {
     System.out.println(1.0 / 2);
 
     System.out.println("2" + "2");
-    System.out.println("2 + 2 =" + (2 + 2));
+    System.out.println("2 + 2 =" + (2 + 2)); */
   }
-}
+
+  public static void hello (String somebody) {
+
+    System.out.println("Hello, " + somebody + "!");
+
+  }
+  }
--- sandbox/src/main/java/myfirstprogram.java
+++ sandbox/src/main/java/myfirstprogram.java
@@ -1,3 +1,31 @@
 public class myfirstprogram {
 
+
+  public static void main(String[] args) {
+    hello("world");
+    hello("User");
+    hello("Jora");
+
+    double len = 5;
+    System.out.println("Площадь квадрата со стороной" + len + " = " + area(len));
+
+    double a = 4;
+    double b = 8;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+
+  }
+
+     public static double area(double a,double b){
+    return a * b;
+     }
+
 }
